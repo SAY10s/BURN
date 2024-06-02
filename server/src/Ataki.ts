d8d09402194df06e3d8736de2d5c62527ef43365
SAY10s <nikodemmajzner@gmail.com>
2024-06-02 16:13:47 +0200

    REMOVE: legacy code CHANGE: moved attack to separate file

---
+++ server/src/Ataki.ts
@@ -0,0 +1,50 @@
+import Character from "./shared/classes/Character.js";
+import DiceManager from "./DiceManager.js";
+import { mozliweLokacjeTrafienia } from "./consts.js";
+
+const atakMieczem = (atakujacy: Character, obronca: Character, ileD6 = 1) => {
+  console.log(`${atakujacy.imie} atakuje mieczem ${obronca.imie}`);
+  let atakRoll = DiceManager.rollD10();
+  let atakSzansa = atakujacy.szanse.atakMieczem + atakRoll;
+  let obrazenia = 0;
+
+  console.log(
+    `Wynik ataku ${atakujacy.imie}: ${atakSzansa} (${atakujacy.szanse.atakMieczem} + ${atakRoll})`,
+  );
+
+  let obronaRoll = DiceManager.rollD10();
+  let obronaSzansa = obronca.szanse.unik + obronaRoll;
+
+  console.log(
+    `Wynik obrony ${obronca.imie}: ${obronaSzansa} (${obronca.szanse.unik} + ${obronaRoll})`,
+  );
+
+  if (atakSzansa > obronaSzansa) {
+    console.log("Atak trafił!");
+    const lokacjaTrafienia = DiceManager.rollD10(false);
+    console.log(
+      `Lokacja trafienia: ${mozliweLokacjeTrafienia[lokacjaTrafienia - 1]}(${lokacjaTrafienia})`,
+    );
+    obrazenia = 0;
+    for (let i = 0; i < ileD6; i++) {
+      obrazenia += DiceManager.rollD6();
+    }
+    console.log(`Obrażenia: ${obrazenia}`);
+    obronca.pz -= obrazenia;
+  } else {
+    console.log("Atak nie trafił");
+  }
+  return {
+    atakujacy: atakujacy.imie,
+    atakRoll,
+    atakMieczem: atakujacy.szanse.atakMieczem,
+    atakSzansa,
+    obronca: obronca.imie,
+    unik: obronca.szanse.unik,
+    obronaSzansa,
+    obronaRoll,
+    obrazenia,
+  };
+};
+
+export default atakMieczem;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,8 @@
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
-import DiceManager from "./DiceManager.js";
 import Character from "./shared/classes/Character.js";
-import { mozliweLokacjeTrafienia } from "./consts.js";
+import atakMieczem from "./Ataki.js";
 
 const app = express();
 const httpServer = createServer(app);
@@ -61,29 +60,6 @@ io.on("connection", (socket) => {
     );
     io.emit("initCharacters", Character.wszystkiePostacie);
   });
-  socket.on("mirekAtakujeTacoMieczem", () => {
-    io.emit(
-      "mirekZaatakowałTaco",
-      atakMieczem(
-        Character.wszystkiePostacie[1],
-        Character.wszystkiePostacie[0],
-        4,
-      ),
-    );
-    io.emit("initCharacters", Character.wszystkiePostacie);
-  });
-  socket.on("tacoAtakujeMirkaZakleciem", () => {
-    io.emit(
-      "tacoZaatakowałMirka",
-      atakZakleciem(
-        Character.wszystkiePostacie[0],
-        Character.wszystkiePostacie[1],
-        4,
-      ),
-    );
-    io.emit("initCharacters", Character.wszystkiePostacie);
-  });
-
   socket.on("disconnect", () => {
     console.log("A user disconnected");
   });
@@ -92,92 +68,3 @@ io.on("connection", (socket) => {
 httpServer.listen(3000, () => {
   console.log("Server is listening on port 3000");
 });
-
-const atakMieczem = (atakujacy: Character, obronca: Character, ileD6 = 1) => {
-  console.log(`${atakujacy.imie} atakuje mieczem ${obronca.imie}`);
-  let atakRoll = DiceManager.rollD10();
-  let atakSzansa = atakujacy.szanse.atakMieczem + atakRoll;
-  let obrazenia = 0;
-
-  console.log(
-    `Wynik ataku ${atakujacy.imie}: ${atakSzansa} (${atakujacy.szanse.atakMieczem} + ${atakRoll})`,
-  );
-
-  let obronaRoll = DiceManager.rollD10();
-  let obronaSzansa = obronca.szanse.unik + obronaRoll;
-
-  console.log(
-    `Wynik obrony ${obronca.imie}: ${obronaSzansa} (${obronca.szanse.unik} + ${obronaRoll})`,
-  );
-
-  if (atakSzansa > obronaSzansa) {
-    console.log("Atak trafił!");
-    const lokacjaTrafienia = DiceManager.rollD10(false);
-    console.log(
-      `Lokacja trafienia: ${mozliweLokacjeTrafienia[lokacjaTrafienia - 1]}(${lokacjaTrafienia})`,
-    );
-    obrazenia = 0;
-    for (let i = 0; i < ileD6; i++) {
-      obrazenia += DiceManager.rollD6();
-    }
-    console.log(`Obrażenia: ${obrazenia}`);
-    obronca.pz -= obrazenia;
-  } else {
-    console.log("Atak nie trafił");
-  }
-  return {
-    atakujacy: atakujacy.imie,
-    atakRoll,
-    atakMieczem: atakujacy.szanse.atakMieczem,
-    atakSzansa,
-    obronca: obronca.imie,
-    unik: obronca.szanse.unik,
-    obronaSzansa,
-    obronaRoll,
-    obrazenia,
-  };
-};
-const atakZakleciem = (atakujacy: Character, obronca: Character, ileD6 = 1) => {
-  console.log(`${atakujacy.imie} atakuje zaklęciem ${obronca.imie}`);
-  let atakRoll = DiceManager.rollD10();
-  let atakSzansa = atakujacy.szanse.atakZakleciem + atakRoll;
-  let obrazenia = 0;
-
-  console.log(
-    `Wynik ataku ${atakujacy.imie}: ${atakSzansa} (${atakujacy.szanse.atakZakleciem} + ${atakRoll})`,
-  );
-
-  let obronaRoll = DiceManager.rollD10();
-  let obronaSzansa = obronca.szanse.unik + obronaRoll;
-
-  console.log(
-    `Wynik obrony ${obronca.imie}: ${obronaSzansa} (${obronca.szanse.unik} + ${obronaRoll})`,
-  );
-
-  if (atakSzansa > obronaSzansa) {
-    console.log("Atak trafił!");
-    const lokacjaTrafienia = DiceManager.rollD10(false);
-    console.log(
-      `Lokacja trafienia: ${mozliweLokacjeTrafienia[lokacjaTrafienia - 1]}(${lokacjaTrafienia})`,
-    );
-    obrazenia = 0;
-    for (let i = 0; i < ileD6; i++) {
-      obrazenia += DiceManager.rollD6();
-    }
-    console.log(`Obrażenia: ${obrazenia}`);
-    obronca.pz -= obrazenia;
-  } else {
-    console.log("Atak nie trafił");
-  }
-  return {
-    atakujacy: atakujacy.imie,
-    atakRoll,
-    atakMieczem: atakujacy.szanse.atakMieczem,
-    atakSzansa,
-    obronca: obronca.imie,
-    unik: obronca.szanse.unik,
-    obronaSzansa,
-    obronaRoll,
-    obrazenia,
-  };
-};
