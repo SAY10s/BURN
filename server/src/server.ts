8ed37ca1333dd5f553d4538badbaa4ba6282e935
SAY10s <nikodemmajzner@gmail.com>
2024-06-02 11:31:29 +0200

    mozliweLokacjeTrafienia są w osobnym pliku

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,19 +3,7 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 import DiceManager from "./DiceManager.js";
 import Character from "./Character.js";
-
-const mozliweLokacjeTrafienia = [
-  "głowa",
-  "korpus",
-  "korpus",
-  "korpus",
-  "Ramię prawe",
-  "Ramię lewe",
-  "Noga prawa",
-  "Noga prawa",
-  "Noga lewa",
-  "Noga lewa",
-];
+import { mozliweLokacjeTrafienia } from "./consts.js";
 
 const app = express();
 const httpServer = createServer(app);
@@ -27,101 +15,6 @@ const io = new Server(httpServer, {
 });
 
 const messages = ["Hello there!", "General kenobi!"];
-Character.wszystkiePostacie = [
-  new Character(
-    "Książe Taco",
-    50,
-    50,
-    50,
-    50,
-    5,
-    {
-      glowa: {
-        wyparowanie: 12,
-        wyparowanieMax: 14,
-      },
-      korpus: {
-        wyparowanie: 5,
-        wyparowanieMax: 8,
-      },
-      lewaReka: {
-        wyparowanie: 6,
-        wyparowanieMax: 8,
-      },
-      prawaReka: {
-        wyparowanie: 6,
-        wyparowanieMax: 8,
-      },
-      lewaNoga: {
-        wyparowanie: 14,
-        wyparowanieMax: 16,
-      },
-      prawaNoga: {
-        wyparowanie: 14,
-        wyparowanieMax: 16,
-      },
-    },
-    {
-      unik: 16,
-      zejscieZLini: 1,
-      atakMieczem: 10,
-      atakPiescia: 16,
-      atakDrzewcowa: 11,
-      atakBitewna: 10,
-      atakKrotka: 10,
-      atakZakleciem: 16,
-      strzalZLuku: 1,
-      strzalZKuszy: 1,
-    },
-  ),
-  new Character(
-    "Mirek",
-    30,
-    30,
-    30,
-    30,
-    1,
-    {
-      glowa: {
-        wyparowanie: 5,
-        wyparowanieMax: 5,
-      },
-      korpus: {
-        wyparowanie: 5,
-        wyparowanieMax: 5,
-      },
-      lewaReka: {
-        wyparowanie: 5,
-        wyparowanieMax: 5,
-      },
-      prawaReka: {
-        wyparowanie: 5,
-        wyparowanieMax: 5,
-      },
-      lewaNoga: {
-        wyparowanie: 5,
-        wyparowanieMax: 5,
-      },
-      prawaNoga: {
-        wyparowanie: 5,
-        wyparowanieMax: 5,
-      },
-    },
-    {
-      unik: 13,
-      zejscieZLini: 1,
-      atakMieczem: 55,
-      atakPiescia: 16,
-      atakDrzewcowa: 11,
-      atakBitewna: 10,
-      atakKrotka: 10,
-      atakZakleciem: 16,
-      strzalZLuku: 1,
-      strzalZKuszy: 1,
-    },
-  ),
-  new Character("Rizzo"),
-];
 
 io.on("connection", (socket) => {
   console.log("Client connected");
