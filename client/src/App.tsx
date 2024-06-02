7ed579c10bb939c999cad87c8ec52e2e2984e400
SAY10s <nikodemmajzner@gmail.com>
2024-06-02 15:34:23 +0200

    Hotfix: removed legacy testing code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,29 +8,6 @@ const App = () => {
   const [input, setInput] = useState("");
   const [characters, setCharacters] = useState<Character[]>([]);
 
-  // ---------------------------------------------
-  // const chooseCharacter = (characterName: string) => {
-  //   // Wysłanie zdarzenia 'chooseCharacter' do serwera z wybranym identyfikatorem postaci
-  //   socket.emit("chooseCharacter", characterName);
-  // };
-  //
-  // // Funkcja do pobrania informacji o wybranej postaci
-  // const getMyCharacter = () => {
-  //   // Wysłanie zdarzenia 'getMyCharacter' do serwera
-  //   socket.emit("getMyCharacter");
-  // };
-
-  // // Nasłuchiwanie na zdarzenie 'myCharacter'
-  // socket.on("myCharacter", (characterName: string) => {
-  //   // Wyświetlanie informacji o wybranej postaci
-  //   console.log("Moja postać to:", characterName);
-  // });
-
-  // Przykładowe użycie funkcji chooseCharacter i getMyCharacter
-  // chooseCharacter("characterId");
-  // getMyCharacter();
-  // ---------------------------------------------
-
   useEffect(() => {
     socket.on("init", (message) => {
       setMessages(() => [...message]);
@@ -41,44 +18,6 @@ const App = () => {
     socket.on("message", (message) => {
       setMessages((prevMessages) => [...prevMessages, message]);
     });
-    socket.on(
-      "mirekZaatakowałTaco",
-      (data: {
-        atakujacy: string;
-        atakRoll: number;
-        atakMieczem: number;
-        atakSzansa: number;
-        obronca: string;
-        unik: number;
-        obronaSzansa: number;
-        obronaRoll: number;
-        obrazenia: number;
-      }) => {
-        setMessages((prevMessages) => [
-          ...prevMessages,
-          `${data.atakujacy} zaatakował ${data.obronca} i zabrał mu ${data.obrazenia} punktów życia!`,
-        ]);
-      },
-    );
-    socket.on(
-      "tacoZaatakowałMirka",
-      (data: {
-        atakujacy: string;
-        atakRoll: number;
-        atakMieczem: number;
-        atakSzansa: number;
-        obronca: string;
-        unik: number;
-        obronaSzansa: number;
-        obronaRoll: number;
-        obrazenia: number;
-      }) => {
-        setMessages((prevMessages) => [
-          ...prevMessages,
-          `${data.atakujacy} zaatakował ${data.obronca} i zabrał mu ${data.obrazenia} punktów życia!`,
-        ]);
-      },
-    );
     socket.on(
       "attackFeedback",
       (data: {
@@ -101,8 +40,6 @@ const App = () => {
 
     return () => {
       socket.off("message");
-      socket.off("tacoZaatakowałMirka");
-      socket.off("mirekZaatakowałTaco");
       socket.off("attackFeedback");
     };
   }, []);
@@ -110,12 +47,6 @@ const App = () => {
   const sendMessage = () => {
     socket.emit("message", input);
     setInput("");
-  };
-  const mirekAtakujeTacoMieczem = () => {
-    socket.emit("mirekAtakujeTacoMieczem");
-  };
-  const tacoAtakujeMirkaZakleciem = () => {
-    socket.emit("tacoAtakujeMirkaZakleciem");
   };
 
   return (
@@ -137,22 +68,6 @@ const App = () => {
         {characters.map((character: any, index: number) => (
           <CharacterCard key={index} character={character} />
         ))}
-      </div>
-      <div>
-        <button
-          onClick={() => {
-            mirekAtakujeTacoMieczem();
-          }}
-        >
-          Mirek atakuje taco mieczem
-        </button>
-        <button
-          onClick={() => {
-            tacoAtakujeMirkaZakleciem();
-          }}
-        >
-          Taci atakuje Mirka zaklęciem
-        </button>
       </div>
     </div>
   );
