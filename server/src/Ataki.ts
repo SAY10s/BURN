993ba94d7ab4f6066cc8f086bb8b712e5c6364eb
SAY10s <nikodemmajzner@gmail.com>
2024-06-04 20:25:59 +0200

    ADD: typ broni, którą się atakauje (WIP, na razie zaklęcie/miecz)

--- server/src/Ataki.ts
+++ server/src/Ataki.ts
@@ -17,52 +17,45 @@ const atak = (data: attackInterface) => {
     // message += `TESTY! ${JSON.stringify(testData)}`;
     //------------------------------------
 
-    console.log(`${data.atakujacy.imie} atakuje mieczem ${data.obronca.imie}`);
-
     let atakRoll = DiceManager.rollD10();
-    let atakSzansa = data.atakujacy.szanse.atakMieczem + atakRoll;
+    //TEMPORARY
+    let atakStatystyka = 2137;
+    let atakSzansa = -1;
+    if (data.zaklecie) {
+      atakStatystyka = data.atakujacy.szanse.atakZakleciem;
+      atakSzansa = data.atakujacy.szanse.atakZakleciem + atakRoll;
+    } else {
+      atakStatystyka = data.atakujacy.szanse.atakMieczem;
+      atakSzansa = data.atakujacy.szanse.atakMieczem + atakRoll;
+    }
+    //--------
     let lokacjaTrafienia = -1;
     let obrazeniaRoll = -1;
     let obrazenia = -1;
     let wyparowanie = -1;
     let mnoznikLokacji = 1;
 
-    console.log(
-      `Wynik ataku ${data.atakujacy.imie}: ${atakSzansa} (${data.atakujacy.szanse.atakMieczem} + ${atakRoll})`,
-    );
-
     let obronaRoll = DiceManager.rollD10();
     let obronaSzansa = data.obronca.szanse.unik + obronaRoll;
 
-    console.log(
-      `Wynik obrony ${data.obronca.imie}: ${obronaSzansa} (${data.obronca.szanse.unik} + ${obronaRoll})`,
-    );
-
-    message += `${data.atakujacy.imie} (${data.atakujacy.szanse.atakMieczem} + ${atakRoll} = ${atakSzansa})
+    message += `${data.atakujacy.imie} (${atakStatystyka} + ${atakRoll} = ${atakSzansa})
            zaatakował ${data.obronca.imie} (${data.obronca.szanse.unik} + ${obronaRoll} = ${obronaSzansa}) 
            użwając ${data.nazwaAtaku}!`;
 
     if (atakSzansa > obronaSzansa) {
-      console.log("Atak trafił!");
-
       // Lokacja trafienia
       lokacjaTrafienia = DiceManager.rollD10(false);
-      console.log(
-        `Lokacja trafienia: ${mozliweLokacjeTrafieniaOdmienionePrzezPrzypadki[lokacjaTrafienia - 1]}(${lokacjaTrafienia})`,
-      );
 
       // obrażenia roll
       for (let i = 0; i < data.ileD6; i++) {
         obrazeniaRoll += DiceManager.rollD6();
       }
-      console.log(`Obrażenia wyrzucone przed wyparowaniem: ${obrazeniaRoll}`);
-
       //wyparowanie
       wyparowanie =
         data.obronca.wyparowanie[
           mozliweLokacjeTrafieniaCamelCase[lokacjaTrafienia - 1]
         ].wyparowanie;
-      console.log("Wyparowanie: ", wyparowanie);
+
       if (obrazeniaRoll > wyparowanie) {
         obrazenia = obrazeniaRoll - wyparowanie;
         if (wyparowanie) {
