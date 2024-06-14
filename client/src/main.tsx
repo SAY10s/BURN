bae7e87271d6c90b4709f64effe14852a58b4448
SAY10s <nikodemmajzner@gmail.com>
2024-06-14 14:48:45 +0200

    ADD: Root element in router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,34 +3,34 @@ import { Provider } from "react-redux";
 import { createRoot } from "react-dom/client";
 import "./index.scss";
 import store from "./typescript/store/store.ts";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import PlayerView from "./typescript/views/BattleViews/PlayerView.tsx";
 import GameMasterView from "./typescript/views/BattleViews/GameMasterView.tsx";
 import DiceTableView from "./typescript/views/DiceTableView.tsx";
 import EditPlayerView from "./typescript/views/EditPlayerView.tsx";
 import AddPlayer from "./typescript/views/AddPlayer.tsx";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <DiceTableView />,
-  },
-  {
-    path: "/edit",
-    element: <EditPlayerView />,
-  },
-  {
-    path: "/player",
-    element: <PlayerView />,
-  },
-  {
-    path: "gm",
-    element: <GameMasterView />,
-  },
-  {
-    path: "add",
-    element: <AddPlayer />,
-  },
-]);
+import Root from "./typescript/views/Root.tsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route path="/" element={<Root />} />
+
+      <Route path="/dice" element={<DiceTableView />} />
+
+      <Route path="gm" element={<GameMasterView />} />
+      <Route path="player" element={<PlayerView />} />
+
+      <Route path="add" element={<AddPlayer />} />
+      <Route path="edit" element={<EditPlayerView />} />
+    </Route>,
+  ),
+);
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
